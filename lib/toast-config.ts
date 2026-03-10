5c756b7f1a9f48fcda82b33bb66695dfa7c7628d
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-03-10 21:59:12 +0800

    create toast config file

---
+++ lib/toast-config.ts
@@ -0,0 +1,53 @@
+export const lightToastOptions = {
+  style: {
+    background: '#ffffff',
+    color: '#171717',
+  },
+  success: {
+    style: {
+      background: '#ffffff',
+      color: '#171717',
+    },
+    iconTheme: {
+      primary: '#16a34a',
+      secondary: '#ffffff',
+    },
+  },
+  error: {
+    style: {
+      background: '#ffffff',
+      color: '#171717',
+    },
+    iconTheme: {
+      primary: '#dc2626',
+      secondary: '#ffffff',
+    },
+  },
+}
+
+export const darkToastOptions = {
+  style: {
+    background: '#1a1a1a',
+    color: '#ededed',
+  },
+  success: {
+    style: {
+      background: '#1a1a1a',
+      color: '#ededed',
+    },
+    iconTheme: {
+      primary: '#16a34a',
+      secondary: '#1a1a1a',
+    },
+  },
+  error: {
+    style: {
+      background: '#1a1a1a',
+      color: '#ededed',
+    },
+    iconTheme: {
+      primary: '#dc2626',
+      secondary: '#1a1a1a',
+    },
+  },
+}
