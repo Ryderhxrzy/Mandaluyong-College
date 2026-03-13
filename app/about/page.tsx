6656a7ae842ba3ef5c92b4a9260a25d8f7d54fc6
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-03-13 14:34:15 +0800

    use realtime about wrapper components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,5 @@
-import AboutBanner from '@/components/about/AboutBanner'
-import AboutKeyStatistics from '@/components/about/AboutKeyStatistics'
-import AboutGoalsPhilosophy from '@/components/about/AboutGoalsPhilosophy'
-import AboutMissionVision from '@/components/about/AboutMissionVision'
-import AboutCoreValuesSection from '@/components/about/AboutCoreValuesSection'
-import AboutWhyChoose from '@/components/about/AboutWhyChoose'
-import AboutJoinCommunity from '@/components/about/AboutJoinCommunity'
+import RealtimeAboutWrapper from '@/components/about/RealtimeAboutWrapper'
 
 export default function About() {
-  return (
-    <div className="min-h-screen bg-white">
-      <AboutBanner />
-      <AboutKeyStatistics />
-      <AboutGoalsPhilosophy />
-      <AboutMissionVision />
-      <AboutCoreValuesSection />
-      <AboutWhyChoose />
-      <AboutJoinCommunity />
-    </div>
-  )
+  return <RealtimeAboutWrapper />
 }
