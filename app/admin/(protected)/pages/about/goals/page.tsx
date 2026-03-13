e5c3894892dd41851ca8f07ead122e129002a2e3
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-03-13 12:34:03 +0800

    change path

--- app/admin/(protected)/pages/about/goals/page.tsx
+++ app/admin/(protected)/pages/about/goals/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import AboutGoalsPhilosophy from '@/components/AboutGoalsPhilosophy'
+import AboutGoalsPhilosophy from '@/components/about/AboutGoalsPhilosophy'
 
 export default function AboutGoalsPage() {
   return (
