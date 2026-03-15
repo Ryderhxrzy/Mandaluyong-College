c55201c28b75c53c7c689aeaf7c3d68ff543b21a
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-03-15 17:11:49 +0800

    create terns and conditions page

---
+++ app/legal/terms-and-conditions/page.tsx
@@ -0,0 +1,10 @@
+import TermsAndCondition from '@/components/terms-and-condition/TermsAndCondition'
+
+export const metadata = {
+  title: 'Terms & Conditions | Mandaluyong College of Science and Technology',
+  description: 'Read our terms and conditions for using MCST website and services',
+}
+
+export default function TermsAndConditionsPage() {
+  return <TermsAndCondition />
+}
