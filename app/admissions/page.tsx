8980b1200c020bf3ff16ecc55ebce3e881343923
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-03-15 13:57:57 +0800

    create admissions page

---
+++ app/admissions/page.tsx
@@ -0,0 +1,5 @@
+import RealtimeAdmissionsWrapper from '@/components/academics/admissions/RealtimeAdmissionsWrapper'
+
+export default function Admissions() {
+  return <RealtimeAdmissionsWrapper />
+}
