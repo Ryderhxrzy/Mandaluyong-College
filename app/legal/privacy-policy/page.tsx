dbd7e88a90c3305c700e867e46c5ecabc5c1fb63
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-03-15 17:11:31 +0800

    create privacy policy page

---
+++ app/legal/privacy-policy/page.tsx
@@ -0,0 +1,10 @@
+import PrivacyPolicy from '@/components/privacy-policy/PrivacyPolicy'
+
+export const metadata = {
+  title: 'Privacy Policy | Mandaluyong College of Science and Technology',
+  description: 'Learn about how we protect and use your information at MCST',
+}
+
+export default function PrivacyPage() {
+  return <PrivacyPolicy />
+}
